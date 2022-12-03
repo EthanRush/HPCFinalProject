cdeb363a1f17badf7b4dcedd6ac9a4ddbaa239aa
EthanRush <rush.e@husky.neu.edu>
2022-12-03 16:25:00 -0500

    String Gather test

--- src/color.h
+++ src/color.h
@@ -16,7 +16,7 @@
 #include <iostream>
 
 
-void write_color(int arr[3], color pixel_color, int samples_per_pixel) {
+void write_color(&std::string pixel, color pixel_color, int samples_per_pixel) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
@@ -33,9 +33,9 @@ void write_color(int arr[3], color pixel_color, int samples_per_pixel) {
     b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
-    arr[0] << static_cast<int>(256 * clamp(r, 0.0, 0.999));
-    arr[1] << static_cast<int>(256 * clamp(g, 0.0, 0.999));
-    arr[2] << static_cast<int>(256 * clamp(b, 0.0, 0.999));
+    pixel   += static_cast<int>(256 * clamp(r, 0.0, 0.999)) + ' '
+            + static_cast<int>(256 * clamp(g, 0.0, 0.999)) + ' '
+            + static_cast<int>(256 * clamp(b, 0.0, 0.999)) + '\n';
 }
 
 
